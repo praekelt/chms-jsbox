a78ab07c9d11c98b2d465dac6aeef74042fc7d72
Rudi Giesler <rudi@praekelt.com>
2016-10-04 15:49:32 +0200

    Better if statement comparison

--- src/ussd_health_worker.js
+++ src/ussd_health_worker.js
@@ -276,7 +276,7 @@ go.app = function() {
                         return errors[name];
                     }
                     names = go.utils.split_parts(content);
-                    if (!(names instanceof Array) || names.length <= 0){
+                    if (!(names instanceof Array) || names.length === 0){
                         return errors[name];
                     }
                     return null;
@@ -287,7 +287,7 @@ go.app = function() {
 
         self.add('check_household_head_name', function(name) {
             names = go.utils.split_parts(self.im.user.answers.state_household_head_name);
-            if (names.length <= 1) {
+            if (names.length === 1) {
                 return self.states.create('state_household_head_surname');
             } else {
                 self.im.user.answers.state_household_head_surname = names.pop();
